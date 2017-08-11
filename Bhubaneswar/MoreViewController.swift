b6694a3f700913cafa367fcd8edd9e7364196f6e
Sambeet Mohapatra <sambeetabc2@gmail.com>
2017-08-11 22:57:51 +0530

    Add files via upload
    Added Bhubaneswar -> xcodeProject

---
+++ Bhubaneswar/MoreViewController.swift
@@ -0,0 +1,61 @@
+//
+//  MoreViewController.swift
+//  Bhubaneswar
+//
+//  Created by Sambeet Mohapatra on 21/06/17.
+//  Copyright © 2017 Sambeet Mohapatra. All rights reserved.
+//
+
+import UIKit
+
+class MoreViewController: UIViewController {
+
+//    @IBOutlet var ScrollView: UIScrollView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        //tap.cancelsTouchesInView = false
+        
+        view.addGestureRecognizer(tap)
+//        ScrollView.contentSize.height = 1000
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+    @IBAction func Submitted(_ sender: Any) {
+        let alertController = UIAlertController(title: "Enquiry", message:
+            "Saved Your Query, Thanks !! ", preferredStyle: UIAlertControllerStyle.alert)
+        alertController.addAction(UIAlertAction(title: "Dismiss", style: UIAlertActionStyle.default,handler: nil))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+    }
+    
+}
